a4e9b5d3c43c04c9b6b7d2eaf448456df4cb12e8
titer1 <titer1@qq.com>
2015-03-21 16:47:35 +0800

    update

---
+++ PAT.cpp
@@ -0,0 +1,22 @@
+// PAT.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+/*
+echo # PAT_DayByDay >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/titer1/PAT_DayByDay.git
+git push -u origin master
+
+*/
+
+//fail
+int main(int argc, _TCHAR* argv[])
+{
+	//hj
+	return 0;
+}
+
